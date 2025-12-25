c2815e4c8d08c92841490d99ac0d72905e5fd87e
Maya Avendano <mayari.avendano@gmail.com>
2025-12-25 18:23:18 +0000

    update join page copy

--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -4,8 +4,14 @@
 
 <div
 	class="mx-auto flex max-w-2xl flex-col items-start justify-center gap-4 px-4 py-16 font-pt-serif [&_a]:text-blue-600 [&_a]:underline">
-	<p>each space is crafted tile by tile, and we are moving in new folks as new parts of veopia become available - fill in the form below to apply to become a fellow resident 🏡</p>
-	<p class="text-sm text-slate-600 italic">note: personal sites will be prioritised over business sites</p>
+	<p>
+		each space is human-made and crafted tile by tile, and new neighbours are added when parts of
+		veopia become available - fill in the form below to apply to become a fellow resident 🏡
+	</p>
+	<p class="text-sm text-slate-600 italic">
+		note: personal sites will be prioritised over business / product sites (filling in the form does
+		not guarantee a spot)
+	</p>
 	<iframe
 		title="Veopia Join Form"
 		src="https://mayari.notion.site/ebd//2cdcb6f1f0318069bccbda7e74edc924"
